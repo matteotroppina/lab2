de39f7df0316db22cef1d204b96db3a9f93903dd
Matteo Troppina <matteo.troppina@tum.de>
2023-11-23 12:59:19 +0100

    Upload New File

---
+++ main.m
@@ -0,0 +1,14 @@
+clear
+close all
+clc
+
+
+lambda = -1;
+t_end = 5; % final time
+x_0 = 1; % intial condition
+dt_vector = [1, 0.5, 0.25, 0.125]; %vector of time steps size (dt)
+
+f = @(t,x) lambda * x; %ODE
+f_sol = @(t) exp(lambda*t); %ODE analytical solution
+
+dahlquist_test(x_0, t_end, dt_vector,f, f_sol);
