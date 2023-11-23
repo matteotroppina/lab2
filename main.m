a457cc89d6476fe73cd6ae6694a8b6767c896e52
Matteo Troppina <matteo.troppina@tum.de>
2023-11-23 15:22:21 +0100

    err_matrix assignment

--- main.m
+++ main.m
@@ -11,4 +11,4 @@ dt_vector = [1, 0.5, 0.25, 0.125]; %vector of time steps size (dt)
 f = @(t,x) lambda * x; %ODE
 f_sol = @(t) exp(lambda*t); %ODE analytical solution
 
-dahlquist_test(x_0, t_end, dt_vector,f, f_sol);
+err_matrix = dahlquist_test(x_0, t_end, dt_vector,f, f_sol);
